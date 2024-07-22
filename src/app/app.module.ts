a223430ef019493a25921eefa60c1a2ce8524f42
Sergei <ryabushenko.serhiy@gmail.com>
2024-07-22 08:34:39 +0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScriptService } from './services/script.service';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    HeaderComponent
+  ],
+  exports: [
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
