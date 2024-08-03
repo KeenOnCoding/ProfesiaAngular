9cd6a9e8a8de1cb8d55035a27e56b3f612313f29
Sergei <ryabushenko.serhiy@gmail.com>
2024-08-03 08:43:43 +0300

    humburger_fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menu: boolean;
+  menuServices: boolean;
+  menuProducts: boolean;
+  menuCompany: boolean;
 
+  ngOnInit(): void {
+    this.menu = false;
+    this.menuServices = false;
+    this.menuProducts = false;
+    this.menuCompany = false;
+  }
+
+  menuClick() {
+    this.menu = !this.menu;
+  }
+  menuServicesClick() {
+    this.menuServices = !this.menuServices;
+  }
+  menuProductsClick() {
+    this.menuProducts = !this.menuProducts;
+  }
+  menuCompanyClick() {
+    this.menuCompany = !this.menuCompany;
+  }
 }
